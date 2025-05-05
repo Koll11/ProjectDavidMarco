680750aaf275e68489e4f4b0c1f38dfc08b6fe08
MarcoSantos <marco7santos@gmail.com>
2025-05-05 15:28:45 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,106 @@
+const animals = [
+    {
+      name: "Lion",
+      image: "https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg",
+      speed: 80,
+      weight: 190,
+      height: 1.2,
+      length: 2.1
+    },
+    {
+      name: "African elephant",
+      image: "https://upload.wikimedia.org/wikipedia/commons/3/37/African_Bush_Elephant.jpg",
+      speed: 40,
+      weight: 6000,
+      height: 3.2,
+      length: 7
+    },
+    {
+      name: "Cheetah",
+      image: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Cheetah_Kruger.jpg",
+      speed: 130,
+      weight: 72,
+      height: 0.9,
+      length: 1.5
+    },
+    {
+      name: "Great White shark",
+      image: "https://upload.wikimedia.org/wikipedia/commons/5/56/White_shark.jpg",
+      speed: 56,
+      weight: 1100,
+      height: 1.3,
+      length: 4.9
+    },
+    {
+      name: "Golden eagle",
+      image: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Aquila_chrysaetos_-_01.jpg",
+      speed: 320,
+      weight: 6.7,
+      height: 0.5,
+      length: 1
+    },
+    {
+      name: "T-Rex",
+      image: "https://upload.wikimedia.org/wikipedia/commons/f/fb/Tyrannosaurus_Rex_Holotype.jpg",
+      speed: 72,
+      weight: 8000,
+      height: 6.1,
+      length: 12
+    },
+    {
+      name: "Velociraptor",
+      image: "https://upload.wikimedia.org/wikipedia/commons/5/53/Velociraptor_dinoguy2.jpg",
+      speed: 40,
+      weight: 20,
+      height: 2,
+      length: 1.8
+    },
+    {
+      name: "Horse",
+      image: "https://upload.wikimedia.org/wikipedia/commons/6/66/White_horse_in_winter.jpg",
+      speed: 71,
+      weight: 420,
+      height: 1.6,
+      length: 2.4
+    },
+    {
+      name: "Nile crocodile",
+      image: "https://upload.wikimedia.org/wikipedia/commons/5/58/NileCrocodile.jpg",
+      speed: 35,
+      weight: 750,
+      height: 0.7,
+      length: 4.2
+    },
+    {
+      name: "Dolphin",
+      image: "https://upload.wikimedia.org/wikipedia/commons/e/e2/Common_dolphin.jpg",
+      speed: 60,
+      weight: 200,
+      height: 0.9,
+      length: 4
+    },
+    {
+      name: "Peregrine falcon",
+      image: "https://upload.wikimedia.org/wikipedia/commons/1/1c/Falco_peregrinus_good_-_Christopher_Watson.jpg",
+      speed: 390,
+      weight: 1.5,
+      height: 0.4,
+      length: 0.58
+    },
+    {
+      name: "Argentinosaurus",
+      image: "https://upload.wikimedia.org/wikipedia/commons/5/5d/Argentinosaurus_model.jpg",
+      speed: 7,
+      weight: 100000,
+      height: 21,
+      length: 40
+    },
+    {
+      name: "Blue whale",
+      image: "https://upload.wikimedia.org/wikipedia/commons/5/52/Blue_Whale_002_body.jpg",
+      speed: 48,
+      weight: 150000,
+      height: 4.9,
+      length: 25
+    }
+];
